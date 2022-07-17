7ca71c34826c0962b8b066069ed88aa2061b9c96
Rajkumar Yadav <rajsknvr@gmail.com>
2022-07-17 11:43:18 +0530

    added socket

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express();
 const http = require('http')
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server)
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+io.on('connection', (socket) => {
+    console.log('A user is connetcd with this applicaions')
+    socket.on('disconnect', () => {
+        console.log('This user is now disconnected................')
+    })
+    socket.on('chat message', (msg) => {
+        console.log('Chat message is : ' + msg);
+        io.emit('chat message', msg);
+    })
 })
 server.listen(3000, () => {
     console.log('App is listening at the port 3000')
