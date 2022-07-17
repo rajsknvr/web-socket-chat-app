3e6e01b7004fbd0ff05c7e3ae1fd88d80334bfac
Rajkumar Yadav <rajsknvr@gmail.com>
2022-07-17 11:06:04 +0530

    initialsetup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const http = require('http')
+const server = http.createServer(app);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+server.listen(3000, () => {
+    console.log('App is listening at the port 3000')
+})
